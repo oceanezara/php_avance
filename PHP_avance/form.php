ce76b61be9054d5d6c0feb7fdbd764a2d6c46435
Océane Raubitschek <oceane@mbp-de-oceane.home>
2021-11-03 11:43:02 +0100

    indentation

--- PHP_avance/form.php
+++ PHP_avance/form.php
@@ -10,26 +10,26 @@
     </div>
     <div>
         <label  for="nom">Nom :</label>
-        <input  type="text"  id="nom"  name="user_name">
+        <input  type="text" id="nom" name="user_name">
     </div>
     <div>
         <label  for="prenom">Prénom :</label>
-        <input  type="text"  id="prenom"  name="prenom">
+        <input  type="text" id="prenom" name="prenom">
     </div>
     <div>
         <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <input  type="email" id="courriel" name="user_email">
     </div>
     <div>
         <label  for="phone">Téléphone :</label>
-        <input  type="number"  id="phone"  name="user_phone">
+        <input  type="number" id="phone" name="user_phone">
     </div>
     <div>
         <label  for="message">Message :</label>
-        <textarea  id="message"  name="user_message"></textarea>
+        <textarea id="message" name="user_message"></textarea>
     </div>
     <div  class="button">
-         <button  type="submit">Envoyer votre message</button>
+         <button type="submit">Envoyer votre message</button>
     </div>
 </form>
 
--- PHP_avance/thanks.php
+++ PHP_avance/thanks.php
@@ -1,12 +1,12 @@
 <html>
-<body>
+    <body>
 
-Merci <?php echo $_POST["prenom"]; ?> de nous avoir contacté à propos de <?php echo $_POST["theme"]; ?>.
-<br>
-Un de nos conseillers vous contactera soit à l'adresse <?php echo $_POST["user_email"]; ?> ou par téléphone au <?php echo $_POST["user_phone"]; ?> dans les plus brefs délais pour traiter votre demande : <br>
+        Merci <?php echo $_POST["prenom"]; ?> de nous avoir contacté à propos de <?php echo $_POST["theme"]; ?>.
+        <br>
+        Un de nos conseillers vous contactera soit à l'adresse <?php echo $_POST["user_email"]; ?> ou par téléphone au <?php echo $_POST["user_phone"]; ?> dans les plus brefs délais pour traiter votre demande : <br>
 
-<?php echo $_POST["user_message"]; ?>
+        <?php echo $_POST["user_message"]; ?>
 
-</body>
+    </body>
 </html>
 
