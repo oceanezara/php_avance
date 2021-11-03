44fd88886bc815499bb6e6265541e37e39a84cf2
Océane Raubitschek <oceane@mbp-de-oceane.home>
2021-11-03 19:24:12 +0100

    validate-form

--- PHP_avance/form.php
+++ PHP_avance/form.php
@@ -1,37 +1,53 @@
-<form  action="thanks.php"  method="post">
-    <div>
-    <label for="theme">Choisir un thème:</label>
-        <select id="theme" name="theme">
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="fiat">Fiat</option>
-            <option value="audi">Audi</option>
-        </select>
-    </div>
-    <div>
-        <label for="nom">Nom :</label>
-        <input type="text" id="nom" name="user_name">
-    </div>
-    <div>
-        <label for="prenom">Prénom :</label>
-        <input type="text" id="prenom" name="prenom">
-    </div>
-    <div>
-        <label for="courriel">Courriel :</label>
-        <input type="email" id="courriel" name="user_email">
-    </div>
-    <div>
-        <label for="phone">Téléphone :</label>
-        <input type="number" id="phone" name="user_phone">
-    </div>
-    <div>
-        <label for="message">Message :</label>
-        <textarea id="message" name="user_message"></textarea>
-    </div>
-    <div  class="button">
-         <button type="submit">Envoyer votre message</button>
-    </div>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <body>
+        <form action="thanks.php"  method="post">
+            <div>
+                <label for="theme">Choisir un thème:</label>
+                <select id="theme" name="theme">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="fiat">Fiat</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </div>
+            <div>
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="user_name">
+            </div>
+            <div>
+                <label for="prenom">Prénom :</label>
+                <input type="text" id="prenom" name="prenom">
+            </div>
+            <div>
+                <label for="courriel">Courriel :</label>
+                <input type="email" id="courriel" name="user_email">
+
+            </div>
+            <div>
+                <label for="phone">Téléphone :</label>
+                <input type="number" id="phone" name="user_phone">
+                <span class="error" <?php $user_phoneErr?>></span>
+
+            </div>
+            <div>
+                <label for="message">Message :</label>
+                <textarea id="message" name="user_message"></textarea>
+            </div>
+            <div  class="button">
+                <button type="submit">Envoyer votre message</button>
+            </div>
+        </form>
+    </body>
+</html>
+
+
 
 
 
--- PHP_avance/thanks.php
+++ PHP_avance/thanks.php
@@ -1,12 +1,84 @@
-<html>
-    <body>
+<?php
+$themeErr = $user_nameErr = $prenomErr = $user_mailErr = $user_phoneErr = $user_messageErr = "";
+$theme = $user_name = $prenom = $user_mail = $user_phone = $user_message = "";
 
-        Merci <?php echo $_POST["prenom"]; ?> de nous avoir contacté à propos de <?php echo $_POST["theme"]; ?>.
-        <br>
-        Un de nos conseillers vous contactera soit à l'adresse <?php echo $_POST["user_email"]; ?> ou par téléphone au <?php echo $_POST["user_phone"]; ?> dans les plus brefs délais pour traiter votre demande : <br>
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 
-        <?php echo $_POST["user_message"]; ?>
+  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (empty($_POST["theme"])){
+        $themeErr = "theme is required";
+    } else {
+        $theme = test_input($_POST["theme"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$theme)) {
+            $themeErr = "Only letters and white space allowed";
+        }
+    }
 
-    </body>
-</html>
+    if (empty($_POST["user_name"])){
+        $user_nameErr = "lastname is required";
+    } else {
+        $user_name = test_input($_POST["user_name"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$user_name)) {
+            $user_nameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if (empty($_POST["user_email"])){
+        $user_mailErr = "Email is required";
+    } else {
+        $user_email = test_input($_POST["user_email"]);
+        if (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
+            $user_mailErr = "Invalid email format";
+        }
+    }
+
+    if (empty($_POST["user_phone"])){
+        $user_phoneErr = "Phone is required";
+    } else {
+        $user_phone = test_input($_POST["user_phone"]);
+        if (!preg_match("/^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/",$user_phone)){
+            $user_phoneErr = "Invalid phone format";
+        }
+    }
+
+    if (empty($_POST["prenom"])){
+        $prenomErr = "prenom is required";
+    } else {
+        $prenom = test_input($_POST["prenom"]);
+    }
+
+    if (empty($_POST["user_message"])){
+        $user_messageErr = "Message is required";
+    } else {
+        $user_message = test_input($_POST["user_message"]);
+        if (!preg_match("/^[a-zA-Z-' ]*$/",$user_message)) {
+            $user_messageErr = "Only letters and white space allowed";
+        }
+    }
+}
+
+echo $themeErr;
+echo "</br>";
+echo $user_nameErr;
+echo "</br>";
+echo $prenomErr;
+echo "</br>";
+echo $user_mailErr;
+echo "</br>";
+echo $user_phoneErr;
+echo "</br>";
+echo $user_messageErr;
+
+if ($theme && $user_name && $user_email && $user_phone && $prenom && $user_message){
+    echo "Merci " . $_POST["prenom"] . " " . $_POST["user_name"] . " de nous avoir contacté à propos de " . $_POST["theme"] . ".</br> </br>" .
+    "Un de nos conseiller vous contactera soit à l'adresse " . $_POST["user_email"] . " ou par téléphone <br> au " 
+    . $_POST["user_phone"] . " dans les plus brefs délais pour traiter votre demande: <br><br>" .
+    $_POST["user_message"];
+} 
+?>
 
